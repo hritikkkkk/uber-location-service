48b4f2ce4a73dca73ec5c99c780706dd984addde
hritik kumar <hritik.7827@gmail.com>
2025-10-02 10:34:31 +0530

    fix(redis): correct latitude and longitude handling in getNearByDrivers

--- src/main/java/com/hritik/location_service/service/RedisLocationServiceImpl.java
+++ src/main/java/com/hritik/location_service/service/RedisLocationServiceImpl.java
@@ -11,13 +11,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class RedisLocationServiceImpl implements LocationService{
+public class RedisLocationServiceImpl implements LocationService {
 
     private static final String DRIVER_GEO_OPS_KEY = "drivers";
     private static final Double SEARCH_RADIUS = 100.0;
 
     private final StringRedisTemplate stringRedisTemplate;
-
 
 
     public RedisLocationServiceImpl(StringRedisTemplate stringRedisTemplate) {
@@ -41,11 +40,11 @@ public class RedisLocationServiceImpl implements LocationService{
     public List<DriverLocationDto> getNearByDrivers(Double latitude, Double longitude) {
         GeoOperations<String, String> geoOps = stringRedisTemplate.opsForGeo();
         Distance radius = new Distance(SEARCH_RADIUS, Metrics.KILOMETERS);
-        Circle within = new Circle(new Point(latitude, longitude), radius);
+        Circle within = new Circle(new Point(longitude, latitude), radius);
 
         GeoResults<RedisGeoCommands.GeoLocation<String>> results = geoOps.radius(DRIVER_GEO_OPS_KEY, within);
         List<DriverLocationDto> drivers = new ArrayList<>();
-        for(GeoResult<RedisGeoCommands.GeoLocation<String>> result : results) {
+        for (GeoResult<RedisGeoCommands.GeoLocation<String>> result : results) {
             Point point = geoOps.position(DRIVER_GEO_OPS_KEY, result.getContent().getName()).get(0);
             DriverLocationDto driverLocation = DriverLocationDto.builder()
                     .driverId(result.getContent().getName())
