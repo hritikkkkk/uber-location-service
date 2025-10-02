3d8af1bb068f07d5b239408c5ac0755dec9b779c
hritik kumar <hritik.7827@gmail.com>
2025-10-02 10:20:30 +0530

    fix(redis): update RedisConnectionFactory bean to use RedisStandaloneConfiguration instead of deprecated setHostName/setPort

--- src/main/java/com/hritik/location_service/config/RedisConfig.java
+++ src/main/java/com/hritik/location_service/config/RedisConfig.java
@@ -4,6 +4,7 @@ package com.hritik.location_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -13,10 +14,11 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-        jedisConFactory.setHostName("localhost");
-        jedisConFactory.setPort(6379);
-        return jedisConFactory;
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("localhost");
+        config.setPort(6379);
+
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
